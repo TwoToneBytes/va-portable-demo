0fe399091889ea867f7b143fd18a9bc3efbbe944
esteban.felix <esteban.felix@servicenow.com>
2024-06-07 11:56:06 -0700

    (fix): dynamic import breaking build

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import './App.css';
-import ServiceNowChat
-    from 'https://support.va-sn.dev/uxasset/externals/now-requestor-chat-popover-app/index.jsdbx?sysparm_substitute=false&uxpcb=1';
 
-const chat = new ServiceNowChat({
-    instance: 'https://support.va-sn.dev',
-    context: {
-        skip_load_history: 1
-    },
-    branding: {},
-    offsetX: 50,
-    offsetY: 50,
-    position: 'right',
-    translations: {
-        'Open dialog': 'Open chat',
-        'Open Chat. {0} unread message(s)': 'Click to open',
-        'Close chat.': 'Click to close',
-    },
-});
+const serviceNowModule = document.createElement('script');
+serviceNowModule.type = 'module';
+serviceNowModule.src = 'https://support.va-sn.dev/uxasset/externals/now-requestor-chat-popover-app/index.jsdbx?sysparm_substitute=false&uxpcb=1';
+serviceNowModule.onload = () => {
+    new window.ServiceNowChat({
+        instance: 'https://support.va-sn.dev',
+        context: {
+            skip_load_history: 1
+        },
+        branding: {},
+        offsetX: 50,
+        offsetY: 50,
+        position: 'right',
+        translations: {
+            'Open dialog': 'Open chat',
+            'Open Chat. {0} unread message(s)': 'Click to open',
+            'Close chat.': 'Click to close',
+        },
+    })
+}
+document.head.appendChild(serviceNowModule);
+
 
 function App() {
     return (
