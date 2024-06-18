ab2caf3e4ed0683616514ef4da430c5526b60800
esteban.felix <esteban.felix@servicenow.com>
2024-06-17 18:17:21 -0700

    (feat): add error page and example routes

--- src/Root.js
+++ src/Root.js
@@ -1,5 +1,6 @@
 import './Root.css';
 import {loadPortableVA} from './portable-va-loader';
+import {Link, Outlet} from "react-router-dom";
 
 const INSTANCE_URL = 'https://support.va-sn.dev';
 const REDIRECT_URL = `${INSTANCE_URL}/sn_va_web_client_login.do?sysparm_redirect_uri=${encodeURIComponent(window.location.href)}`;
@@ -8,7 +9,20 @@ loadPortableVA({INSTANCE_URL, REDIRECT_URL});
 function Root() {
     return (<div className="App">
         <header className="App-header">
+            <nav>
+                <ul>
+                    <li>
+                        <Link to={`/public`}>Public Virtual Agent</Link>
+                    </li>
+                    <li>
+                        <Link to={`/sso`}>Virtual Agent SSO Login</Link>
+                    </li>
+                </ul>
+            </nav>
         </header>
+        <div id="root">
+            <Outlet/>
+        </div>
     </div>);
 }
 
--- src/index.js
+++ src/index.js
@@ -6,11 +6,19 @@ import Root from './Root';
 import reportWebVitals from './reportWebVitals';
 
 import './index.css';
+import ErrorPage from "./ErrorPage";
 
 const router = createBrowserRouter({
     path: '/',
-    element: Root
-});
+    element: Root,
+    errorElement: ErrorPage,
+    children: [
+        {
+            path: '/public',
+            element: () => <div>Public</div>
+        }
+    ]
+},);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
@@ -21,4 +29,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(console.log);
